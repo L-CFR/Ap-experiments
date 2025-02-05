ada20ac141d30b70eb03b928fdaa36206534c041
Pratyush Sharma <142144538+L-CFR@users.noreply.github.com>
2025-02-05 13:12:52 +0530

    Create jump game.cpp

---
+++ jump game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int mr = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > mr) {
+                return false;
+            }
+            mr = max(mr, i + nums[i]);
+            if (mr >= n - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
